6e07f0ea25417731f864349ccffb07c4d7ce21ed
Stuart Nelson <stuartnelson3@gmail.com>
2014-02-22 20:41:54 -0500

    first commit

---
+++ server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+    "github.com/codegangsta/martini"
+    "github.com/codegangsta/martini-contrib/render"
+    "path/filepath"
+    "regexp"
+    "fmt"
+    "os"
+)
+
+func main() {
+    m := martini.Classic()
+    m.Use(martini.Static("songs"))
+    m.Use(render.Renderer(render.Options{
+        Layout:     "layout",
+        Delims: render.Delims{"{[{", "}]}"},
+        Extensions: []string{".html"}}))
+
+    m.Get("/", func(r render.Render) {
+        songs := Songs()
+        r.JSON(200, songs)
+    })
+
+    m.Run()
+}
+
+func ClosureHackage(payload *Payload) func(s string, f os.FileInfo, err error) error {
+    return func(path string, f os.FileInfo, err error) error {
+        re := regexp.MustCompile(`\.mp3$`)
+        if match := re.FindString(path); match != "" {
+            payload.Songs = append(payload.Songs, path)
+            fmt.Println(path[6:])
+        }
+        return nil
+    }
+}
+
+func Songs() Payload {
+    songs := make([]string, 0)
+    payload := Payload{Songs: songs}
+    FindMp3s := ClosureHackage(&payload)
+    filepath.Walk(".", FindMp3s)
+    return payload
+}
+
+type Payload struct {
+    Songs []string
+}
